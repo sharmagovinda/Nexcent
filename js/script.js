0bb18882c98673a88659bad9f4b9906b9dea9a84
Govinda sharma <ss.dev.454@gmail.com>
2025-12-04 22:11:39 +0545

    footer section added

--- js/script.js
+++ js/script.js
@@ -1,44 +1,5 @@
 jQuery(document).ready(function ($) {
 
-  //  for sticky header
-
-  const stickyHeader = function () {
-    const height = $(window).scrollTop();
-    const header = $('.site-header');
-
-    if (height > 60) {
-      header.addClass('stickyHeader');
-    } else {
-      header.removeClass('stickyHeader');
-    }
-  };
-
-  const paddingEl2 = function () {
-    const headerHeight = $('.site-header').innerHeight();
-    $('.banner').css('padding-top', headerHeight);
-  };
-
-  stickyHeader();
-
-  setTimeout(function () {
-    stickyHeader();
-    paddingEl2();
-  }, 1000);
-
-  $(window).scroll(function () {
-    stickyHeader();
-  });
-
-  $(window).resize(function () {
-    stickyHeader();
-    paddingEl2();
-
-    setTimeout(function () {
-      stickyHeader();
-      paddingEl2();
-    }, 1000);
-  });
-
   //  for Hamberger Menu
 
   let hambugerMenu = () => {
